a7dcf362e167b8a42188ef9f1eea64e8621d7227
Thijs Schreijer <thijs@thijsschreijer.nl>
2022-02-10 17:23:11 +0100

    feat(credentials) TokenFileWebIdentityCredentials use env from config

--- src/resty/aws/credentials/TokenFileWebIdentityCredentials.lua
+++ src/resty/aws/credentials/TokenFileWebIdentityCredentials.lua
@@ -4,9 +4,11 @@
 local readfile = require("pl.utils").readfile
 local xml = require("pl.xml")
 
-local DEFAULT_SESSION_NAME = "session@lua-resty-aws"
-local ROLE_ARN = os.getenv("AWS_ROLE_ARN")
-local TOKEN_FILE = os.getenv("AWS_WEB_IDENTITY_TOKEN_FILE")
+
+local global_config = require("resty.aws.config").global
+local AWS_ROLE_ARN = global_config.role_arn
+local AWS_WEB_IDENTITY_TOKEN_FILE = global_config.web_identity_token_file
+local AWS_ROLE_SESSION_NAME = global_config.role_session_name or "session@lua-resty-aws"
 
 
 -- Create class
@@ -20,21 +22,21 @@ TokenFileWebIdentityCredentials.__index = TokenFileWebIdentityCredentials
 -- @tparam table opts options table, only listing additional fields to the `Credentials` class.
 -- @tparam[opt=AWS_WEB_IDENTITY_TOKEN_FILE env var] string opts.token_file filename of the token file
 -- @tparam[opt=AWS_ROLE_ARN env var] string opts.role_arn arn of the role to assume
--- @tparam[opt='session@lua-resty-aws'] string opts.session_name session name
+-- @tparam[opt=AWS_ROLE_SESSION_NAME env var or 'session@lua-resty-aws'] string opts.session_name session name
 function TokenFileWebIdentityCredentials:new(opts)
   local self = Super:new(opts)  -- override 'self' to be the new object/class
   setmetatable(self, TokenFileWebIdentityCredentials)
 
   opts = opts or {}
   self.token_file = assert(
-    opts.token_file or TOKEN_FILE,
+    opts.token_file or AWS_WEB_IDENTITY_TOKEN_FILE,
     "either 'opts.token_file' or environment variable 'AWS_WEB_IDENTITY_TOKEN_FILE' must be set"
   )
   self.role_arn = assert(
-    opts.role_arn or ROLE_ARN,
+    opts.role_arn or AWS_ROLE_ARN,
     "either 'opts.role_arn' or environment variable 'AWS_ROLE_ARN' must be set"
   )
-  self.session_name = opts.session_name or DEFAULT_SESSION_NAME
+  self.session_name = opts.session_name or AWS_ROLE_SESSION_NAME
 
   return self
 end
@@ -44,7 +46,7 @@ end
 function TokenFileWebIdentityCredentials:refresh()
   if not self.sts then
     -- instantiate on first use. Cannot do this in the constructor, since the
-    -- constructor is called when instaniating an AWS instance (creating a loop).
+    -- constructor is called when instantiating an AWS instance (creating a loop).
     -- That's because this credentials class is part of the "CredentialProviderChain"
     local AWS = require "resty.aws"
     local aws = AWS()
