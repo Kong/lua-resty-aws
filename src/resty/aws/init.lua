dd4e38c26bb0ae7b702c9226c73d3c13a83b99a1
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2023-09-19 17:28:49 +0300

    chore(*): just remove variables that are not needed

--- src/resty/aws/init.lua
+++ src/resty/aws/init.lua
@@ -15,7 +15,7 @@ local tablex = require("pl.tablex")
 local lookup_helper = function(self, key)  -- signature to match __index meta-method
   if type(key) == "string" then
     local lckey = key:lower()
-    for k,v in pairs(self) do
+    for k in pairs(self) do
       if type(k) == "string" and k:lower() == lckey then
         error(("key '%s' not found, did you mean '%s'?"):format(key, k), 2)
       end
@@ -193,7 +193,7 @@ do
   -- @param service service-instance being created; field `aws` is the aws instance,
   -- `config` is the service instance config, `api` the service api.
   function AWS.configureEndpoint(service)
-    for i, key in ipairs(derivedKeys(service)) do
+    for _, key in ipairs(derivedKeys(service)) do
 
       local region_rule_config = aws_config.region.rules[key]  --> contains regions templates
       if type(region_rule_config) == 'string' then
